a1ecc5ddcc605d57159feb77af95686ceb2149e1
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-26 21:44:05 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -50,7 +50,7 @@ SELECT * FROM animals;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-UPDATE animals SET species = 'pokemon' WHERE species = 'unspecified';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 SELECT * FROM animals;
 COMMIT;
 SELECT * FROM animals;
