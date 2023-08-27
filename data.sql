6143b9598a08188c6956e165fe4012234fca2522
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-27 06:48:12 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -45,3 +45,10 @@ UPDATE animals SET owner_id = (
     WHEN name IN ('Angemon', 'Boarmon') THEN 5
   END
 );
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+    ('William Tatcher', 45, '2000-04-23'),
+    ('Maisy Smith', 26, '2019-01-17'),
+    ('Stephanie Mendez', 64, '1981-05-04'),
+    ('Jack Harkness', 38, '2008-06-08');
+    
