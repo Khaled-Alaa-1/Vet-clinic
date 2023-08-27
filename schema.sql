1ed139c03a838578e15b0a3d59416e402bb775e1
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-27 06:46:23 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -37,3 +37,16 @@ CREATE TABLE vets (
     age INTEGER,
     date_of_graduation DATE
 );
+
+CREATE TABLE specializations (
+    vet_id INTEGER REFERENCES vets(id),
+    species_id INTEGER REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+    animal_id INTEGER REFERENCES animals(id),
+    vet_id INTEGER REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
+);
