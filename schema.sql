6b278040c82bbab0b7e53923838a2513a012d433
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-08-27 05:52:08 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,26 +1,32 @@
-/* Database schema to keep the structure of entire database. */
+postgres=# CREATE DATABASE vet_clinic;
+postgres=# \c vet_clinic
+vet_clinic=# CREATE TABLE animals (
+    id serial PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(10, 2)
+);
 
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
 
 CREATE TABLE owners (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  full_name VARCHAR(255),
-  age INTEGER
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
+    age INTEGER
 );
 
 CREATE TABLE species (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(255)
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
 );
 
-CREATE TABLE animals (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(55),
-  date_of_birth DATE,
-  escape_attempts INTEGER,
-  neutered BOOLEAN,
-  weight_kg DECIMAL(5,2),
-  species_id INTEGER,
-  owner_id INTEGER,
-  FOREIGN KEY (species_id) REFERENCES species(id),
-  FOREIGN KEY (owner_id) REFERENCES owners(id)
-);
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
